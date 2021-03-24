a3ee64b9458bd04e391df99b297ad40578aa241f
shaunvan119 <shaunvan@live.com.au>
2021-03-24 14:19:24 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(someValue) {
+if (someValue <= 400) {return "This one is on me!"}
+ else if (someValue > 2000 && someValue < 2499) {return "I will gladly take your thirty bucks."}
+ else if (someValue > 2500) {return "No can do."}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  const phrase = city === "NYC" ? "Ok, sounds good." : "No go."
+  return phrase
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let phrase
+  switch (tip) {
+    case 'generous': phrase = 'Thank you so much.';
+    break;
+    case 'not as generous': phrase = 'Thank you.';
+    break;
+    default: phrase = 'Bye.'
+  }
+  return phrase
+
 }
